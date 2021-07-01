e90905d0071be6afad87b9a83e1932bf617ea3b2
mychi2506 <86784520+mychi2506@users.noreply.github.com>
2021-07-01 21:30:43 +0700

    Update and rename config.js to mychi.js

---
+++ mychi.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'tình yêu CỦA em aaa !',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'không nhấp vào là đục',
+    title: 'trên thế giới có hơn 7 tỉ nụ cười 😙 nhưng mà với em nụ cười anh mới là đẹp nhất : >>> ',
+    desc: 'thế nên là làm ơn hãy để em giúp anh giữ nụ cười ấy trên môi',
+    btnYes: ' ukiiii babii <33',
+    btnNo: ' nâu nâu nâu nâu :<<',
+    question:'anh ơi anh ơi, anh cho em chăm sóc anh đến khi đầu bạc răng long nhaaa :333 ',
+    btnReply: ' send this answer <3',
+    reply: 'okieeeeee <33333333',
+    mess: 'dzaaaaa 🥰. em thưn anh nhìu nhìu 😘😘',
+    messDesc: 'gặp nhau anh cho em thơm má nhaa :> thơm xong ở má rồi thơm tới môi :>> nhaaaaaa',
+    btnAccept: 'Okiiiii lun <3' 
+}
