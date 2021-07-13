ba6059c2073fccec26206dc3231f6f2d6c417514
chee-2506 <86784520+chee-2506@users.noreply.github.com>
2021-07-13 22:46:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,10 +6,9 @@ const CONFIG = {
     desc: 'anh sẽ là "ngôi sao" sáng nhất của riêng em chứ ? ',
     btnYes: 'ukiii babiii <33',
     btnNo: 'à ừm nâu nâu nâu :<',
-    question:'chuyện rằng...em có một điều ước, em và anh, đôi ta sẽ bên nhau mãi mãi từ nay về sau và về sau nữa <3 nha anhh ',
+    question:  'Tuyết Nhi à, em chỉ muốn nói là em yêu anh, yêu anh nhiều lắm. em có một mong muốn nho nhỏ...em và anh, hai mình sẽ bên nhau mãi mãi từ nay về sau và về sau nữa <3 nha anhh ',
     btnReply: 'đồng ý đi cho bóp zú <3 thiệt luôn hong nói xạo',
     reply: 'coáaaaaaaaa <33333333',
     messDesc: 'gặp nhau cho em thơm má nha...thơm từ hai má thơm đến đôi môi.',
     btnAccept: 'Okiiiii lun <3'
-    mess: 'Tuyết Nhi à, em chỉ muốn nói là em yêu anh, yêu anh nhiều lắm',
 }
