03e282c1508b631c2d37eab778601492adb3cad1
mychi2506 <86784520+mychi2506@users.noreply.github.com>
2021-07-13 22:11:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 const CONFIG = {
-    introTitle: 'xin chào tình yêu !',
+    introTitle: 'xin chàoooo bồ em :>> !',
     introDesc: `i'm a bee. do you want to be my honey ?`,
-    btnIntro: 'có hoặc bị đục',
-    title: ' khi em nhìn vào mắt anh, em nhận ra sao trên trời đều là hàng giả 😙',
-    desc: 'anh sẽ là "ngôi sao" sáng nhất của riêng em chứ nhỉ  ? ',
+    btnIntro: 'có hoặc kó',
+    title: ' khi nhìn vào mắt anh, em nhận ra sao trên trời đều là hàng giả 😙',
+    desc: 'anh sẽ là "ngôi sao" sáng nhất của riêng em chứ ? ',
     btnYes: 'ukiii babiii <33',
     btnNo: 'à ừm nâu nâu nâu :<',
-    question:'chuyện rằng...em có một điều ước, em và anh, đôi ta sẽ mãi bên nhau về sau và về sau nữa <3',
-    btnReply: 'đồng ý đi cho bóp zú <3',
+    question:'chuyện rằng...em có một điều ước, em và anh, đôi ta sẽ bên nhau mãi mãi từ nay về sau và về sau nữa <3 nha anhh ',
+    btnReply: 'đồng ý đi cho bóp zú <3 thiệt luôn hong nói xạo',
     reply: 'coáaaaaaaaa <33333333',
-    mess: 'ỏoooooo 🥰. em yêu anh 😘😘',
-    messDesc: 'gặp nhau cho em thơm má...thơm từ hai má thơm đến đôi môi.',
+    messDesc: 'gặp nhau cho em thơm má nha...thơm từ hai má thơm đến đôi môi.',
     btnAccept: 'Okiiiii lun <3'
+    mess: 'Tuyết Nhi à, em chỉ muốn nói là em yêu anh, yêu anh nhiều nhắm',
 }
